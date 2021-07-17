7a3cc85761caf35e3851741b7c6a77f879174b3f
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-17 12:00:37 +0300

    Update task17JulB.cpp

--- task17JulB.cpp
+++ task17JulB.cpp
@@ -10,7 +10,7 @@ float result = yplusplus - y;
 if(yplusplus >y)
 cout << "your profit about :" << result << endl;
 else 
-cout << "There is a loss of value about :" << result << endl;
+cout << "There is a loss of value about :" << -1*result << endl;
  
 return 27;
 }
